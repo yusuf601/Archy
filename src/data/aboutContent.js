5b413ecf886e40b82798944136292e260a018b1b
yusuf601 <yusufmuhyusuh@gmail.com>
2026-08-28 09:13:50 +0700

    feat: add About visual assets

--- src/data/aboutContent.js
+++ src/data/aboutContent.js
@@ -1,4 +1,9 @@
-const localMediaPoster = (filename) => `/src/assets/images/media/${filename}`
+import theMartianPoster from '../assets/images/media/the-martian.jpg'
+import leaveTheWorldBehindPoster from '../assets/images/media/leave-the-world-behind.jpg'
+import carsPoster from '../assets/images/media/cars.jpg'
+import reply1988Poster from '../assets/images/media/reply-1988.jpg'
+import fromPoster from '../assets/images/media/from.jpg'
+import theNightAgentPoster from '../assets/images/media/the-night-agent.jpg'
 
 export const aboutProfile = Object.freeze({
     name: 'Muh Yusuf',
@@ -22,42 +27,42 @@ export const onScreenItems = Object.freeze([
         title: 'The Martian',
         year: 2015,
         type: 'film',
-        poster: localMediaPoster('the-martian.jpg'),
+        poster: theMartianPoster,
         alt: 'Poster for The Martian',
     }),
     Object.freeze({
         title: 'Leave the World Behind',
         year: 2023,
         type: 'film',
-        poster: localMediaPoster('leave-the-world-behind.jpg'),
+        poster: leaveTheWorldBehindPoster,
         alt: 'Poster for Leave the World Behind',
     }),
     Object.freeze({
         title: 'Cars',
         year: 2006,
         type: 'film',
-        poster: localMediaPoster('cars.jpg'),
+        poster: carsPoster,
         alt: 'Poster for Cars',
     }),
     Object.freeze({
         title: 'Reply 1988',
         year: 2015,
         type: 'series',
-        poster: localMediaPoster('reply-1988.jpg'),
+        poster: reply1988Poster,
         alt: 'Poster for Reply 1988',
     }),
     Object.freeze({
         title: 'FROM',
         year: 2022,
         type: 'series',
-        poster: localMediaPoster('from.jpg'),
+        poster: fromPoster,
         alt: 'Poster for FROM',
     }),
     Object.freeze({
         title: 'The Night Agent',
         year: 2023,
         type: 'series',
-        poster: localMediaPoster('the-night-agent.jpg'),
+        poster: theNightAgentPoster,
         alt: 'Poster for The Night Agent',
     }),
 ])
